46d2649a8fd4456610c0ce3e034a0f96ed98faca
Dasffafa <1605302380@qq.com>
2023-01-25 23:20:17 +0800

    Added more paintings

--- src/main/java/com/dasffafa/fantasia/common/painting/FantasiaPaintings.java
+++ src/main/java/com/dasffafa/fantasia/common/painting/FantasiaPaintings.java
@@ -6,6 +6,7 @@ import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegistryObject;
 
+@SuppressWarnings("unused")
 public class FantasiaPaintings {
     public static final DeferredRegister<Motive> PAINTINGS = DeferredRegister.create(ForgeRegistries.PAINTING_TYPES, Fantasia.MOD_ID);
 
@@ -15,4 +16,8 @@ public class FantasiaPaintings {
     public static final RegistryObject<Motive> JY_AND_MESTERIOUS_PERSON = PAINTINGS.register("mysterious_person_and_maid", () -> new Motive(32,48));
 
     public static final RegistryObject<Motive> STAGE_PHOTO = PAINTINGS.register("stage_photo", () -> new Motive(32, 64));
+    public static final RegistryObject<Motive> LEMON_BADGE = PAINTINGS.register("lemon_badge", () -> new Motive(64, 64));
+    public static final RegistryObject<Motive> RIO_BADGE = PAINTINGS.register("rio_badge", () -> new Motive(32, 32));
+    public static final RegistryObject<Motive> IMMORTAL_CHURCH_BADGE = PAINTINGS.register("immortal_church_badge", () -> new Motive(32, 32));
+    public static final RegistryObject<Motive> GOLD_KELA_TRUCK = PAINTINGS.register("gold_kela_truck", () -> new Motive(32, 16));
 }
